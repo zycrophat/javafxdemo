dea0261ff1cdba6f3f9c126c95ade72153603e7b
Andreas Steffan <andreas.steffan@mailbox.org>
2019-07-09 20:23:40 +0200

    using javafx gradle plugin instead of custom javafx lib resolution

--- javafxdemo-fxview/src/main/java/steffan/javafxdemo/fxview/base/JavaFXApplication.java
+++ javafxdemo-fxview/src/main/java/steffan/javafxdemo/fxview/base/JavaFXApplication.java
@@ -3,17 +3,27 @@ package steffan.javafxdemo.fxview.base;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
+import java.util.concurrent.CountDownLatch;
+
 public class JavaFXApplication extends Application {
 
     private static FXUIViewManager fxViewManager;
 
+    private static final CountDownLatch initCountDownLatch = new CountDownLatch(1);
+
     public static void initialize(FXUIViewManager fxViewManager) {
         JavaFXApplication.fxViewManager = fxViewManager;
-        new Thread(Application::launch).start();
+        new Thread(Application::launch, "JavaFXApplication").start();
+        try {
+            initCountDownLatch.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void start(Stage primaryStage) throws Exception {
         fxViewManager.setPrimaryStage(primaryStage);
+        initCountDownLatch.countDown();
     }
 }
